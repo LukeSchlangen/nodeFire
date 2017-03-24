f6f56396578ca5fea5c546d37744b2d40ef0a754
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-24 10:52:04 -0500

    close #13 by updating to rows.length

--- server/routes/private-data.js
+++ server/routes/private-data.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res){
         res.sendStatus(500);
       }else{
         pg.connect(connectionString, function(err, client, done){
-          if(clearanceLevelQueryResult.rowCount === 0) {
+          if(clearanceLevelQueryResult.rows.length === 0) {
             // If the user is not in the database, return a forbidden error status
             console.log('No user found with that email. Have you added this person to the database? Email: ', req.decodedToken.email);
             res.sendStatus(403);
